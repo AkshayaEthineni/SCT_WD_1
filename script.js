b9e0a3ca4782a52fd97ca54a90b3208b6d1e8089
AkshayaEthineni <Akshayae600@gmail.com>
2025-02-04 10:56:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get the navigation menu
+const nav = document.querySelector('.nav');
+
+// Add an event listener to the window scroll event
+window.addEventListener('scroll', () => {
+    // Check if the window has scrolled down
+    if (window.scrollY > 50) {
+        // Add
+
